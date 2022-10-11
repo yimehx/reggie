8536368d8324fa813d1e8fbc35afb287c7615ee0
yimehx <1277987376@qq.com>
2022-10-11 15:53:38 +0800

    使用redis方式缓存验证码

--- src/main/java/com/mpl/reggie/config/RedisConfig.java
+++ src/main/java/com/mpl/reggie/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.mpl.reggie.config;
 
 import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -8,6 +9,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
     public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory connectionFactory){
         RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
         //默认的Key序列化器为： JdkSerializationRedisSerializer
--- src/main/java/com/mpl/reggie/controller/UserController.java
+++ src/main/java/com/mpl/reggie/controller/UserController.java
@@ -4,19 +4,19 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.mpl.reggie.common.R;
 import com.mpl.reggie.entity.User;
 import com.mpl.reggie.service.UserService;
-import com.mpl.reggie.utils.SMSUtils;
 import com.mpl.reggie.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
@@ -25,6 +25,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * 发送验证码
@@ -42,8 +45,11 @@ public class UserController {
             log.info("code：{}",code);
             //调用阿里云提供的短信服务API完成发送短信
 //            SMSUtils.sendMessage("","",phone,code);
-            //将生成的验证码保存到Session
-            session.setAttribute(phone,code);
+//            //将生成的验证码保存到Session
+//            session.setAttribute(phone,code);
+            //将生成的验证码缓存到redis,设置有效期为5分钟
+            redisTemplate.opsForValue().set(phone,code,5, TimeUnit.MINUTES);
+
             return R.success("手机验证码短信发送成功");
         }
         return R.error("短信发送失败");
@@ -63,8 +69,10 @@ public class UserController {
         String phone = map.get("phone").toString();
         //获取验证码
         String code = map.get("code").toString();
-        //从Session中获取保存的验证码
-        Object codeInSession = session.getAttribute(phone);
+//        //从Session中获取保存的验证码
+//        Object codeInSession = session.getAttribute(phone);
+        //从redis中获取缓存的验证码
+        Object codeInSession = redisTemplate.opsForValue().get(phone);
         //进行验证码的比对
         if (null != codeInSession && codeInSession.equals(code)){
             //如果能够对比成功，说明登陆成功
@@ -79,6 +87,8 @@ public class UserController {
                 userService.save(user);
             }
             session.setAttribute("user",user.getId());
+            //如果用户登录成功，删除Redis中缓存的验证码
+            redisTemplate.delete(phone);
             return R.success(user);
         }
         return R.error("登录失败");
