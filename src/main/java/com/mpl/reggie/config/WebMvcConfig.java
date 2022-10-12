1ce75645245dfe67ac8c9028d6d40a3e15086f3b
yimehx <1277987376@qq.com>
2022-10-12 23:35:52 +0800

    加入Swagger文档功能

--- src/main/java/com/mpl/reggie/config/WebMvcConfig.java
+++ src/main/java/com/mpl/reggie/config/WebMvcConfig.java
@@ -1,16 +1,48 @@
 package com.mpl.reggie.config;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import com.mpl.reggie.common.JacksonObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
 @Configuration
+@EnableSwagger2 //开启swagger文档功能
+@EnableKnife4j  //开启swagger文档功能
 public class WebMvcConfig extends WebMvcConfigurationSupport{
+
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //扫描所有接口
+                .apis(RequestHandlerSelectors.basePackage("com.mpl.reggie.controller"))
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                //描述接口文档标题
+                .title("瑞吉外卖")
+                //文档版本
+                .version("1.0")
+                //文档注释
+                .description("瑞吉外卖接口文档")
+                .build();
+    }
     /**
      * 设置静态资源映射
      * @param registry
@@ -20,6 +52,8 @@ public class WebMvcConfig extends WebMvcConfigurationSupport{
 //        registry.addResourceHandler("backend/**").addResourceLocations("classpath:/backend/");
 //        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
         registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     /**
